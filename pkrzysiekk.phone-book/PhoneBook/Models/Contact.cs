e0d9cc237f8014248911878bee77fef3d1b6a983
pkrzysiek <pkrzysiek24@gmail.com>
2025-03-11 13:48:48 +0100

    Ready for PR

---
+++ pkrzysiekk.phone-book/PhoneBook/Models/Contact.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBook.Models;
+
+public class Contact
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string email { get; set; }
+    public string PhoneNumber { get; set; }
+}
---
+++ pkrzysiekk.phone-book/PhoneBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using PhoneBook.Models;
+
+namespace PhoneBook;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Menu menu = new Menu();
+        menu.Show();
+    }
+}
